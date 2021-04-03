6af61e8f70f6ea15b90fd0602de366c3869d172c
Suey Chen <suey.chen.16@gmail.com>
2021-04-03 18:56:37 -0400

    initial commit

--- HW-unit4-6-p2/index.js
+++ HW-unit4-6-p2/index.js
@@ -2,44 +2,72 @@
 
 //Using what you did with Objects perdiod 2 from the slides, create a personal profile. Instead of Products you will need to create a personal profile. Initialize Constructors and More!
 //Step 1
-
+function Person(name, age, pronouns, favoriteColor) {
+  this.name = name;
+  this.age = age;
+  this.pronouns = pronouns;
+  this.favoriteColor = favoriteColor;
+};
 
 
 
 //Step 2
-
+let person1 = new Person("Judy", 31, ['she', 'her'], "red");
 
 
 
 //Step 3
-
-
+let person2 = new Person("Jonathon", 24, ['he', 'him'], "black");
+let person3 = new Person("Randy", 15, ['she', 'her'], "orange")
+let person4 = new Person("Jenn", 29, ['she', 'her'], "black")
 
 //Step 4
-
+function Pet(type, breed, name) {
+  this.type = type;
+  this.breed = breed;
+  this.name = name;
+};
 
 //Step 5
+function Person(name, age, pronouns, favoriteColor, pet) {
+  this.name = name;
+  this.age = age;
+  this.pronouns = pronouns;
+  this.favoriteColor = favoriteColor;
+  this.pet = pet
+};
 
 
 //Step 6
-
+let dog1 = new Pet("dog", "golden retriever", "Hamlet");
+let person5 = new Person("Stephen", 30, ['he', 'him'], "blue", dog1);
+console.log(person5.pet.name);
 
 // Step 7
-
-
-// Step 8
+person5.hairColor = 'brown';
 
 
 //.Create
 
 //Step 1
-
+let Teacher = {
+  firstName: "Amy",
+  lastName: "Johnson",
+  subject: ['math', 'science'],
+  getFullName() {
+    console.log(`${this.firstName} ${this.lastName}`)
+  }
+}
 
 //Step 2
-
+let teacher1 = Object.create(Teacher)
+teacher1.getFullName()
 
 //Step 3
-
+let teacher2 = Object.create(Teacher)
+teacher2.firstName = "Jessica"
+teacher2.lastName = "Brown"
+teacher2.getFullName()
 
 
 //Array vs Objects
